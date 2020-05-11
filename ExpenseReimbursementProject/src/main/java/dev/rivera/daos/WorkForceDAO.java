6fd2ecf6c9f376f5f0965049b50156deb0fc6005
Jesse Rivera <35208196+codiene@users.noreply.github.com>
2020-05-11 00:06:44 -0400

    deleted wrong project

--- ExpenseReimbursementProject/src/main/java/dev/rivera/daos/WorkForceDAO.java
+++
@@ -1,7 +0,0 @@
-package dev.rivera.daos;
-
-import dev.rivera.entities.WorkForce;
-
-public interface WorkForceDAO {
-	public WorkForce updateWorkForce(WorkForce wf);
-}
--- ExpenseReimbursementProject/src/main/java/dev/rivera/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package dev.rivera.services;
-
-import dev.rivera.entities.*;
-
-public interface UserService {
-	public Employee loginEmployee(String username,String password);
-	public Manager loginManager(String username,String password);
-}
